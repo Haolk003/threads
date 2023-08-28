eb6590d1f23b84397841edb390a33897e1591b6e
haolk003 <nguyenquochaolop91@gmail.com>
2023-08-28 12:56:18 +0700

    add

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
